86b93bc0526b21148922419a751c9c556b19538c
alex <desorbaix@free.fr>
2020-08-18 17:16:02 +0200

    mise en place du router dom

--- src/App/App.js
+++ src/App/App.js
@@ -4,15 +4,21 @@ import './App.css';
 import Mybutton from './Mybutton2/Mybutton2';
 import Tchat from './Tchat/Tchat';
 import TchatLogin from './TchatLogin/TchatLogin';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <Router>
       <div className="App">
-        {/* <Tchat user={{id:1,name:'Alex'}}></Tchat> */}
-        <TchatLogin >
-          <Link to="/tchat">Connexion</Link>
-        </TchatLogin>
+        <Switch>
+          <Route exact path="/tchat">
+            <Tchat user={{ id: 1, name: 'Alex' }}></Tchat>
+          </Route>
+          <Route path="/">
+            <TchatLogin >
+              <Link to="/tchat">Connexion</Link>
+            </TchatLogin>
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
